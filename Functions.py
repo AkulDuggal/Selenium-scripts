e56b442a1aeb1c589663e684e8f309f12f82b6f1
AkulDuggal <akul.duggal46@gmail.com>
2024-10-21 12:47:59 +0530

    new case for signup added

--- Functions.py
+++ Functions.py
@@ -18,6 +18,13 @@ def initialize_driver(access_type):
     return driver
 
 def enter_info(driver, email, password):
+    if "sign-up" in driver.current_url:
+        Messagebox=WebDriverWait(driver,10).until(
+        EC.element_to_be_clickable((By.XPATH,"(//div[@class='css-175oi2r r-1phboty'])[2]"))
+    ).click()
+    else: 
+        print("No box to click")
+
     input_fields = WebDriverWait(driver, 10).until(
         EC.presence_of_all_elements_located((By.CSS_SELECTOR, "input[data-testid='text-input-flat']"))
     )
